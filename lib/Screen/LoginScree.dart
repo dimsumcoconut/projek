740c7b615aad8fa2d9fe52315e61fad697d27d53
ed.ginaa <113158996+edginajlvni@users.noreply.github.com>
2024-10-01 15:54:24 +0700

    vv login

---
+++ lib/Screen/LoginScree.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class LogInScreen extends StatelessWidget {
+  const LogInScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      appBar: AppBar(
+      title: Text("Log In"),
+      ),
+    );
+  }
+}
